492bc9ffd156a3369cc9bc7c24290128c6b87b99
zhannach <zhanna.chaykovska@gmail.com>
2023-06-03 23:02:31 +0300

    feat: save cart to LocalStorage with redux-persist

--- redux/reduxProvider.tsx
+++ redux/reduxProvider.tsx
@@ -3,7 +3,16 @@
 import { ReactNode } from "react";
 import { store } from "./store";
 import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
-export default function ReduxProvider({ children }: {children: ReactNode}) {
-  return <Provider store={store}>{children}</Provider>;
+export default function ReduxProvider({ children }: { children: ReactNode }) {
+  const persistor = persistStore(store);
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
--- redux/store.ts
+++ redux/store.ts
@@ -1,14 +1,40 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import shopReducer from "./slices/shopSlice";
 import cartReducer from "./slices/cartSlice";
 import userReducer from "./slices/userSlice";
 
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cartItems", "user"],
+};
+
+export const appReducer = combineReducers({
+  shop: shopReducer,
+  cartItems: cartReducer,
+  user: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, appReducer);
+
 export const store = configureStore({
-  reducer: {
-    shop: shopReducer,
-    cartItems: cartReducer,
-    user: userReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
